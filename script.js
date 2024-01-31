527a707f0ce2e027e4517408dac6c843012158ba
Joeliton Oliveira <joelitonoliveira@MacBook-Air-de-Joeliton.local>
2024-01-31 13:07:40 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function togglemode() {
+  const html = document.documentElement
+/*
+  if(html.classlist.contains('light')){
+  html.classlist.remove('light')
+  } else {
+    html.classlist.add('light')
+  }
+}
+*/
+html.classList.toggle('light')
+}
